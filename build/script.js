5c74fc1a99a13cfaa7f89a67d23ea036d78f3e66
gonzika <hziben@gmail.com>
2021-01-08 17:11:39 +0200

    del

--- build/script.js
+++
@@ -1,7 +0,0 @@
-// function closeMenu() {
-//     document.getElementById('toggler-input').click();
-//     document.getElementById('toggler').classList.add('leftbar');
-//     setTimeout(() => {
-//         document.getElementById('toggler').classList.remove('leftbar');
-//     }, 1000);
-// }
